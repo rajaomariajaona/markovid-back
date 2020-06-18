a91a1b3849340622bd1a47fce1b5b9a35afe2df9
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-06-18 13:47:56 +0300

    fix nom

--- rules.pl
+++ rules.pl
@@ -1,6 +1,6 @@
 cas_suspect(Nombre,Lieu).
-cas_confirmee(Nombre,Lieu).
-zone_rouge(Lieu):- cas_confirmee(Nombre,Lieu).
+cas_confirme(Nombre,Lieu).
+zone_rouge(Lieu):- cas_confirme(Nombre,Lieu).
 zone_rouge(Lieu):- cas_suspect(Nombre,Lieu),Nombre>10.
 zone_jaune(Lieu):- cas_suspect(Nombre,Lieu),Nombre=<10.
 zone_risque(Lieu,Heure_debut, Heure_fin):- aglomeration(Lieu,Heure_debut,Heure_fin).
